64a715b8770ae8ddfb33813263149629df98d6b1
Priyabrat Swain <priyabratswain888@gmail.com>
2025-06-30 20:27:35 +0530

    Add configuration and script chnages to upload image in azure blob storage

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const { log } = require('console');
 const express = require('express');
 const multer = require('multer');
 const app = express();
 const hbs = require('hbs');
 const path = require('path');
-const fs = require("fs");
-// const dotenv = require('dotenv');
+const { BlobServiceClient} = require("@azure/storage-blob");
+const dotenv = require('dotenv');
 
-// dotenv.config();
+dotenv.config();
 const port = process.env.PORT || 3000;
 const viewsPath = path.join(__dirname, '/templates/views');
 const partialsPath = path.join(__dirname, '/templates/component');
 
-const storage = multer.diskStorage({
-  destination: function (req, image, cb) {
-    cb(null, './public/images')
-  },
-  filename: function (req, image, cb) {
-    const filename = Date.now() + '-' + image.fieldname;
-    cb(null, filename );
-  }
-})
+const sas = process.env.AZURE_BLOB_SAS_CONNECTION_STRING;
+const account = process.env.ACCOUNT_NAME;
+const containerName = process.env.CONTAINER_NAME;
 
-const upload = multer({ storage: storage })
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net?${sas}`);
+const containerClient = blobServiceClient.getContainerClient(containerName);
 
 // Set up view engine
 app.set('view engine', 'hbs');
@@ -30,8 +28,6 @@ app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 app.use(express.static('public'));
-
-
 
 app.get('/', (req, res) => {
     res.render('index', {
@@ -41,20 +37,31 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/upload', upload.single('image'), (req, res) => {
+app.post('/upload', upload.single('image'), async (req, res) => {
   if (!req.file) {
     return res.status(400).send('No file uploaded');
   }else{
-    console.log('File uploaded successfully:', req.file.filename);
+    try{  
+    const blobName = req.file.originalname; 
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+    const uploadBlobResponse = await blockBlobClient.upload(req.file.buffer, req.file.size);
+    res.status(200).send('File uploaded successfully');
+    }catch(err){
+      return res.status(500).send('Error uploading file');
+    }
+    
   }
 });
 
-app.get('/view', (req, res) => {
-  let filenames = fs.readdirSync('./public/images');
+app.get('/view', async (req, res) => {
   let images = [];
-  filenames.forEach((file) => {
-    images.push('/images/' + file);
-  });
+  let i = 1;
+  const blobs = containerClient.listBlobsFlat();
+  for await (const blob of blobs) {
+    const blobUrl = `https://${account}.blob.core.windows.net/${containerName}/${blob.name}?${sas}`;
+    images.push(blobUrl);
+  }
+  
   res.render('viewImage', {
     data: {
       title: "View Image",
